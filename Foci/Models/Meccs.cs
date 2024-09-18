e524e94a6ea8d2433fbb8399ec7c47505c9e5aa9
balintfy.peter <balintfy.peter@BURG-1E03-05>
2024-09-18 11:03:38 +0200

    Add project files.

---
+++ Foci/Models/Meccs.cs
@@ -0,0 +1,15 @@
+﻿namespace Foci.Models
+{
+    public class Meccs
+    {
+        public int Id { get; set; }
+        public int Fordulo { get; set; }
+        public int HazaiVeg { get; set; }
+        public int VendegVeg { get; set; }
+        public int HazaiFordulo { get; set; }
+        public int VendegFordulo { get; set; }
+        public string? HazaiCsapat { get; set; }
+        public string? VendegCsapat { get; set; }
+
+    }
+}
